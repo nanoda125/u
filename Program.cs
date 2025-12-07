89db5e3587176fbea696c333e2010e3e2d8729b7
りとるん <ritorun788@gmail.com>
2025-12-08 06:15:49 +0900

    プロジェクトファイルを追加。

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DxLibDLL;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ウィンドウモードで起動するように設定
+        DX.ChangeWindowMode(DX.TRUE);
+
+        // Dxlib の初期化
+        DX.DxLib_Init();
+        // 描画先を裏画面に設定
+        DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+
+        // メインループ
+        while (DX.ProcessMessage() == 0)
+        {
+            // 画面をクリア
+            DX.ClearDrawScreen();
+            // 文字を描画
+            DX.DrawString(100, 100, "Hello World", DX.GetColor(255, 255, 255));
+            // 裏画面の内容を表画面に反映
+            DX.ScreenFlip();
+        }
+
+        // Dxlib の終了処理
+        DX.DxLib_End();
+    }
+}
